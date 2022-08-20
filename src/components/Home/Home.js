7ca2ca7aada16f062cb7b7860105d17238b60a40
Borislav Tiukenov <reksor4@gmail.com>
2022-08-21 02:34:52 +0300

    new looks

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,10 +10,10 @@ const Home=({characters})=>{
     return(
         <section id="welcome-world">
       <div className="welcome-message">
-        <h2>ALL new games are</h2>
-        <h3>Only in GamesPlay</h3>
+        <h2></h2>
+        <h3>Create your heroes</h3>
       </div>
-      <img src="./images/four_slider_img01.png" alt="hero" />
+      <img src="./images/scarlet_witch_wanda_transparent_png_by_purpleaxell_df6pew1-fullview.png" alt="hero" />
       <div id="home-page">
         <h1>Latest Games</h1>
         {/* Display div: with information about every game (if any) */}
